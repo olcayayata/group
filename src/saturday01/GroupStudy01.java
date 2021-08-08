00d8358309574c246547a12e610594766e509ace
olcayayata <olcayayata4@gmail.com>
2021-08-08 00:28:24 -0400

    change

--- src/saturday01/GroupStudy01.java
+++ src/saturday01/GroupStudy01.java
@@ -4,5 +4,6 @@ public class GroupStudy01 {
     public static void main(String[] args) {
         System.out.println("Olcay");
         System.out.println("Frank");
+        System.out.println("Mary");
     }
 }
